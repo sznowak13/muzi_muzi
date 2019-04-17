eb5c6e012f94588bd47b0417a5c012577157101f
Szymon Nowak <sznowak13@gmail.com>
2019-04-17 17:10:22 +0200

    Added example dburi class

---
+++ test_data_generator/connection/dburi.example.py
@@ -0,0 +1,6 @@
+class DBURI:
+    user = "username"
+    password = "password"
+    host = "localhost"
+    name = "muzi_muzi"
+    connection_string = f"postgresql://{user}:{password}@{host}/{name}"
