c7bbb91d1c442108dd6ed1ca8800d98b13237411
Szymon Nowak <sznowak13@gmail.com>
2019-05-20 14:03:15 +0200

    Added some query params to get_adverts endpoint

--- web_app/muzi_muzi/urls.py
+++ web_app/muzi_muzi/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.adverts, name='adverts'),
+    path('get_adverts', views.adverts, name='adverts'),
 ]
--- web_app/web_app/urls.py
+++ web_app/web_app/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('muzi-muzi', include('muzi_muzi.urls'))
+    path('muzi-muzi/', include('muzi_muzi.urls'))
 ]
