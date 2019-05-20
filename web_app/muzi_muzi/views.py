5d9c7f40b0e91258ad73da9ba002b7447b352470
Szymon Nowak <sznowak13@gmail.com>
2019-05-20 11:01:33 +0200

    Added simple json endpoint with adverts

--- web_app/muzi_muzi/views.py
+++ web_app/muzi_muzi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
+from .models import AdvertListView
 
-# Create your views here.
+
+def adverts(request):
+    sample = AdvertListView.objects.all()
+
+    data = serializers.serialize('json', sample)
+
+    return HttpResponse(data, content_type='application/json')
