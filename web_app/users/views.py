a728cbff953147b730da06eada652fb5ded6ce50
pKraszewska <paula.kraszewska@gmail.com>
2019-05-22 10:28:13 +0200

    endpoint view for userListView added

--- web_app/users/views.py
+++ web_app/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
+from .models import UserListView
 
-# Create your views here.
+
+def users(request):
+    limit = request.GET.get('limit', 20)
+    if not isinstance(limit, int) and not limit.isdigit():
+        response = {'error': f'requested with incorrect query param limit: {limit}',
+                    'description': f'Endpoint {request.path} with query params {request.GET.dict()}'}
+        return JsonResponse(response)
+
+    limit = int(limit)
+    filter_by = request.GET.get('filter', '-city')
+    sample = UserListView.objects.order_by(filter_by)[:limit]
+    data = serializers.serialize('json', sample)
+
+    return HttpResponse(data, content_type='application/json')
