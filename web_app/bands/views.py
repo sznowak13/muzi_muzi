f1d40f83be23d797c89e68b13e9b43e856daa6d6
pKraszewska <paula.kraszewska@gmail.com>
2019-05-22 10:44:48 +0200

    view for bands added

--- web_app/bands/views.py
+++ web_app/bands/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
+from .models import Band
 
-# Create your views here.
+
+def bands(request):
+    limit = request.GET.get('limit', 20)
+    if not isinstance(limit, int) and not limit.isdigit():
+        response = {'error': f'requested with incorrect query param limit: {limit}',
+                    'description': f'Endpoint {request.path} with query params {request.GET.dict()}'}
+        return JsonResponse(response)
+
+    limit = int(limit)
+    filter_by = request.GET.get('filter', '-band_id')
+    sample = Band.objects.order_by(filter_by)[:limit]
+    data = serializers.serialize('json', sample)
+
+    return HttpResponse(data, content_type='application/json')
