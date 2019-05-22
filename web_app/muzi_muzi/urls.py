8ab65f4168c921d8b7904e1c20758aaec0097853
pKraszewska <paula.kraszewska@gmail.com>
2019-05-22 10:45:12 +0200

    path for bands view added

--- web_app/muzi_muzi/urls.py
+++ web_app/muzi_muzi/urls.py
@@ -6,4 +6,5 @@ app_name = 'muzi_muzi'
 urlpatterns = [
     path('get_adverts', views.adverts, name='adverts'),
     path('get_users', views.users, name='users'),
+    path('get_bands', views.bands, name='bands'),
 ]
