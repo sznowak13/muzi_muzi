9675e438ddf9d74dad066570726f4441144c4bce
Szymon Nowak <sznowak13@gmail.com>
2019-05-22 19:30:51 +0200

    Started using rest framework

--- web_app/web_app/urls.py
+++ web_app/web_app/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adverts/', include('adverts.urls')),
     path('users/', include('users.urls')),
     path('bands/', include('bands.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('muzi_muzi.urls'))
 ]
