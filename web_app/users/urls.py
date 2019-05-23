d8082102e4eff25715d00f597d6224d128b24f8c
pKraszewska <paula.kraszewska@gmail.com>
2019-05-23 12:59:57 +0200

    unused imports deleted

--- web_app/users/urls.py
+++ web_app/users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import SimpleRouter
-from rest_framework.urlpatterns import format_suffix_patterns
 
 router = SimpleRouter()
 router.register('users', views.UserViewSet)
