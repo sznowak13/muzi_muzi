f5447afc9b30eb3360b1a89870d9363363480a1a
Basia <bachna22@gmail.com>
2019-05-29 12:13:14 +0200

    Created tabs

---
+++ front/src/News.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Adverts from "./Adverts";
+import { Container } from "react-bootstrap";
+import { Tabs } from "antd";
+
+export default class News extends Component {
+  render() {
+    const { TabPane } = Tabs;
+
+    function callback(key) {
+      console.log(key);
+    }
+    return (
+      <Container className="news">
+        <Tabs onChange={callback} type="card">
+          <TabPane tab="Adverts" key="1">
+            <Adverts />
+          </TabPane>
+          <TabPane tab="Bands" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="Users" key="3">
+            Content of Tab Pane 3
+          </TabPane>
+        </Tabs>
+      </Container>
+    );
+  }
+}
