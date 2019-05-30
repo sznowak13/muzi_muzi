cca9d2a634c21df718dc21cd65f0ee2a0a542322
Szymon Nowak <sznowak13@gmail.com>
2019-05-30 19:06:37 +0200

    Changed band serialzer and added member srializer
    band serializer now has related fields so it
    muts be cosen while creating new band
    member serializer is used to serialize band members
    as we dont need every detail of the member

--- web_app/bands/serializers.py
+++ web_app/bands/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
 from .models import Band
+from muzi_muzi.models import Genre, City
+from users.models import Users
 
 
 class BandsSerializer(serializers.HyperlinkedModelSerializer):
     # adverts = serializers.PrimaryKeyRelatedField(many=True, queryset=Advert.objects.all())
-    members = serializers.HyperlinkedIdentityField(view_name='users-detail', many=True)
-    genres = serializers.StringRelatedField(many=True)
-    city = serializers.StringRelatedField()
+    members = serializers.HyperlinkedRelatedField(view_name='users-detail', many=True, queryset=Users.objects.all())
+    genres = serializers.SlugRelatedField(many=True, slug_field='name', queryset=Genre.objects.all())
+    city = serializers.SlugRelatedField(slug_field='name', queryset=City.objects.all())
 
     class Meta:
         model = Band
         fields = ('band_id', 'url', 'name', 'city', 'year_founded',
                   'photo', 'homepage', 'description', 'members', 'genres')
+
+
+class MemberSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Users
+        fields = ('url', 'user_id', 'username', 'email')
