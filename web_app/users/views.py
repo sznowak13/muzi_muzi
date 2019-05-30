38ab93f6c111c0d5f1893c98db0a6008066f3ae5
Szymon Nowak <sznowak13@gmail.com>
2019-05-30 19:09:53 +0200

    Users now can be searched

--- web_app/users/views.py
+++ web_app/users/views.py
@@ -1,11 +1,13 @@
 from .models import Users
 from .serializers import UsersSerializer, UsersRegisterSerializer
-from rest_framework import generics, mixins, viewsets
+from rest_framework import generics, mixins, viewsets, filters
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username', 'email')
 
 
 class UserRegisterView(mixins.CreateModelMixin,
