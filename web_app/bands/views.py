4afe56609b351b2f722138027153a410a00bcd34
Szymon Nowak <sznowak13@gmail.com>
2019-05-30 19:09:10 +0200

    Added custom notify_members endpoint for bands
    it sends mail with provided message to all band members

--- web_app/bands/views.py
+++ web_app/bands/views.py
@@ -1,10 +1,44 @@
 from .models import Band
-from .serializers import BandsSerializer
+from .serializers import BandsSerializer, MemberSerializer
+from rest_framework.decorators import action
 from rest_framework import viewsets
+from rest_framework.response import Response
+from .permissions import IsMemberOrReadOnly
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 
-class BandViewSet(viewsets.ReadOnlyModelViewSet):
+def generate_emails(subject, message, members):
+    text_message = strip_tags(message)
+    mails = [
+        EmailMultiAlternatives(subject, text_message, from_email='noreply@muzi.muzi', to=[member.email])
+        for member in members
+    ]
+    for msg in mails:
+        msg.attach_alternative(message, 'text/html')
+    return mails
+
+
+def send_mails(mails):
+    delivered = 0
+    for message in mails:
+        delivered += message.send()
+    return delivered
+
+
+class BandViewSet(viewsets.ModelViewSet):
     queryset = Band.objects.all()
     serializer_class = BandsSerializer
 
-
+    @action(detail=True, permission_classes=[IsMemberOrReadOnly], methods=['post'])
+    def notify_members(self, request, pk=None):
+        band = self.get_object()
+        user = request.user
+        subject = f'Notification from {user}'
+        message = render_to_string('email_template.html',
+                                   {'band': band, 'user': user, 'message': request.data['message']})
+        messages = generate_emails(subject, message, band.members.all())
+        delivered = send_mails(messages)
+        users = MemberSerializer(band.members.all(), context={'request': request}, many=True)
+        return Response({'members_notified': users.data, 'message': request.data['message'], 'delivered': delivered})
--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -156,3 +156,8 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
 )
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'muzi.muzi.django@gmail.com'
+EMAIL_HOST_PASSWORD = 'pumpernikiel123'
