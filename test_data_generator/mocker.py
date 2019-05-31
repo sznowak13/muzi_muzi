22214a7d4afc20e9306065ddca451d2af0613353
Szymon Nowak <sznowak13@gmail.com>
2019-06-01 00:26:10 +0200

    Created TableSetting class, moved user input to fn
    Tabe setting is a simple class for saving specific table
    setting provided by user from std input

--- test_data_generator/mocker.py
+++ test_data_generator/mocker.py
@@ -30,6 +30,27 @@ def get_bool_from_input(inpt: str, valid_confirms):
     return False
 
 
+class TableSetting:
+
+    def __init__(self, to_generate: int, generate: bool, clear: bool):
+        self.to_generate = to_generate
+        self.generate = generate
+        self.clear = clear
+
+
+def get_table_input(prompt, confirms=None):
+    if confirms is None:
+        confirms = CONFIRMS
+    to_generate = input(prompt)
+    if not to_generate or not to_generate.isdigit():
+        to_generate = 0
+    else:
+        to_generate = int(to_generate)
+    generate = to_generate > 0
+    clear = False if not generate else get_bool_from_input(input("Clear table first? (y/N) "), confirms)
+    return TableSetting(to_generate, generate, clear)
+
+
 def main():
     print("Welcome to test data mocker for muzi_muzi database!")
 
