d63f8abea2a91c1df99194b6debae6cf77a31873
Szymon Nowak <sznowak13@gmail.com>
2019-05-31 18:15:48 +0200

    Added custom action on bands endpoint

--- web_app/bands/views.py
+++ web_app/bands/views.py
@@ -1,10 +1,17 @@
 from .models import Band
 from .serializers import BandsSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class BandViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Band.objects.all()
     serializer_class = BandsSerializer
+    latest_num = 10
 
-
+    @action(detail=False)
+    def latest(self, request):
+        queryset = self.get_queryset().order_by('-created')[:self.latest_num]
+        serializer = BandsSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
