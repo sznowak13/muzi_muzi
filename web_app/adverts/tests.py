ddc1aedbcff2c49f48430313eb3b31513c4a832d
Szymon Nowak <sznowak13@gmail.com>
2019-06-03 20:27:38 +0200

    Removed messed up tests
    we must think about testing it better

--- web_app/adverts/tests.py
+++ web_app/adverts/tests.py
@@ -2,10 +2,5 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Advert
-
-
-class AdvertsTest(APITestCase):
-
-    def test_latest(self):
-
-        url = reverse('adverts-latest')
+from users.models import Users
+from bands.models import Band
