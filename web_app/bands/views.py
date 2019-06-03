a35554c7ee88a2e7ec86db235a4afce748b598a1
Szymon Nowak <sznowak13@gmail.com>
2019-06-03 14:49:22 +0200

    Added latest mixins to adverts,bands,users

--- web_app/bands/views.py
+++ web_app/bands/views.py
@@ -1,20 +1,9 @@
 from .models import Band
 from .serializers import BandsSerializer
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from muzi_muzi.views import LatestViewMixIn
 
 
-class BandViewSet(viewsets.ReadOnlyModelViewSet):
+class BandViewSet(viewsets.ReadOnlyModelViewSet, LatestViewMixIn):
     queryset = Band.objects.all()
     serializer_class = BandsSerializer
-    latest_num = 10
-
-    @action(detail=False)
-    def latest(self, request):
-        latest_num = request.query_params.get('num', self.latest_num)
-        if not isinstance(latest_num, int) and not latest_num.isdigit():
-            return Response({'error': f'Wrong num parameter, was not a number. (num={latest_num})'})
-        queryset = self.get_queryset().order_by('created')[:int(latest_num)]
-        serializer = BandsSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
--- web_app/users/views.py
+++ web_app/users/views.py
@@ -1,16 +1,18 @@
 from .models import Users
 from .serializers import UsersSerializer, UsersRegisterSerializer
-from rest_framework import generics, mixins, viewsets, filters
+from rest_framework import mixins, viewsets, filters
 from rest_framework.permissions import AllowAny
 from .permissions import IsUserOrReadOnly
+from muzi_muzi.views import LatestViewMixIn
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet, LatestViewMixIn):
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('username', 'email')
     permission_classes = (IsUserOrReadOnly,)
+    latest_by = 'date_joined'
 
 
 class UserRegisterView(mixins.CreateModelMixin,
