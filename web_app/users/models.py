d675d031075fdc4429e0dfd8e34bc8a57c87b6c5
Szymon Nowak <sznowak13@gmail.com>
2019-06-04 17:33:27 +0200

    Tweaks in db schema, new migrations

--- web_app/users/models.py
+++ web_app/users/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+DEFAULT_ROLE_ID = 1
+
+
 class Users(AbstractUser):
     user_id = models.AutoField(primary_key=True)
-    role = models.ForeignKey('muzi_muzi.Role', models.DO_NOTHING, blank=True, null=True)
-    city = models.ForeignKey('muzi_muzi.City', models.DO_NOTHING, blank=True, null=True)
+    role = models.ForeignKey('muzi_muzi.Role', models.SET_NULL, null=True, default=DEFAULT_ROLE_ID)
+    city = models.ForeignKey('muzi_muzi.City', models.SET_NULL, blank=True, null=True)
     photo_url = models.CharField(max_length=255, blank=True, null=True)
-    video = models.ForeignKey('muzi_muzi.Videos', models.DO_NOTHING, blank=True, null=True)
+    video = models.ForeignKey('muzi_muzi.Videos', models.SET_NULL, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     genres = models.ManyToManyField('muzi_muzi.Genre')
     professions = models.ManyToManyField('muzi_muzi.Profession')
