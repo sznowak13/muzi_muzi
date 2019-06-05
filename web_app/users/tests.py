0b53783a4f54427e6067cfceccbcd021bbedb0ed
Szymon Nowak <sznowak13@gmail.com>
2019-06-05 17:54:28 +0200

    New tests for users!

--- web_app/users/tests.py
+++ web_app/users/tests.py
@@ -17,9 +17,9 @@ class TestUsers(APITestCase):
         url = reverse('users-latest')
         response = self.client.get(url)
         json_response = json.loads(response.content.decode('utf-8'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 4)
-        self.assertEqual(json_response, [
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data) == 4
+        assert json_response == [
             {'user_id': 4, 'url': 'http://testserver/users/4/', 'first_name': 'Ddam', 'last_name': 'Ddamowski',
              'username': 'Test4', 'role': None, 'city': None, 'photo_url': None, 'video': None, 'description': None,
              'genres': [], 'professions': [], 'bands': [], 'adverts': []},
@@ -32,4 +32,39 @@ class TestUsers(APITestCase):
             {'user_id': 1, 'url': 'http://testserver/users/1/', 'first_name': 'Adam', 'last_name': 'Adamowski',
              'username': 'Test1', 'role': None, 'city': None, 'photo_url': None, 'video': None, 'description': None,
              'genres': [], 'professions': [], 'bands': [], 'adverts': []},
-        ])
+        ]
+
+    def test_user_register(self):
+        Users.objects.all().delete()  # Deleting users from setUp
+        url = reverse('register-list')
+        resp = self.client.post(url, data={
+            'username': 'test_user',
+            'password': 'tester',
+            'password2': 'tester',
+            'email': 'email@test.pl',
+            'email2': 'email@test.pl'
+        })
+        assert resp.status_code == status.HTTP_201_CREATED
+        assert Users.objects.count() == 1  # As 4 users are created on setup
+        assert Users.objects.get().username == 'test_user'
+        assert Users.objects.get().is_active is False
+
+    def test_user_register_with_not_same_password_email(self):
+        url = reverse('register-list')
+        resp = self.client.post(url, data={
+            'username': 'test_user',
+            'password': 'tester',
+            'password2': 'not_same',
+            'email': 'email@test.pl',
+            'email2': 'not_same@test.pl'
+        })
+        json_response = json.loads(resp.content.decode('utf-8'))
+        assert resp.status_code == status.HTTP_400_BAD_REQUEST
+        assert json_response == {
+            "password": [
+                "Password must match"
+            ],
+            "email": [
+                "Emails must match"
+            ]
+        }
