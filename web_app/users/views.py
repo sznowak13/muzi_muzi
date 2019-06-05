13d4c39567648416e851f4528aa7825e348024dd
Szymon Nowak <sznowak13@gmail.com>
2019-06-05 20:32:56 +0200

    created basic endpoint for verifying mails

--- web_app/users/views.py
+++ web_app/users/views.py
@@ -1,7 +1,9 @@
 from .models import Users
 from .serializers import UsersSerializer, UsersRegisterSerializer
-from rest_framework import mixins, viewsets, filters
+from rest_framework import mixins, viewsets, filters, status
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from .permissions import IsUserOrReadOnly
 from muzi_muzi.views import LatestViewMixIn
 
@@ -20,3 +22,10 @@ class UserRegisterView(mixins.CreateModelMixin,
     queryset = Users.objects.all()
     serializer_class = UsersRegisterSerializer
     permission_classes = (AllowAny,)
+
+    @action(detail=False, methods=['get'])
+    def verify_email(self, request):
+        token = request.data.get('key')
+        if not token:
+            return Response({"error": "Missing verification token"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"ok": "ok"})
