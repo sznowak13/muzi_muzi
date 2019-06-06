b907e3c13bc636efdf91dddd18d39fc1d1fa7866
pKraszewska <paula.kraszewska@gmail.com>
2019-06-06 14:11:18 +0200

    added drop down in side bar with form for login

--- front/src/LoggedOutSidebar.js
+++ front/src/LoggedOutSidebar.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { Sidenav, Icon, Divider } from "rsuite";
+import { Sidenav, Icon, Divider, Dropdown } from "rsuite";
 import { Button } from "react-bootstrap";
+import { Form, FormGroup, FormControl, ControlLabel, HelpBlock, ButtonToolbar } from 'rsuite';
+
 
 export default class LoggedOutSidebar extends Component {
   render() {
     return (
-      <div style={{ width: 250 }}>
+      <div style={{ width: "100%" }}>
         <Sidenav>
           <Sidenav.Body>
             <Sidenav.Header>
@@ -16,22 +18,43 @@ export default class LoggedOutSidebar extends Component {
               </div>
             </Sidenav.Header>
             <div className="welcome-sign">
-              Don't have an account? It's free! Sign up and join us.
+              <p>Don't have an account?</p>
+              <p>It's free! </p>
+              <p>Sign up and join us.</p>
               <p>{<Icon icon="heart" />}</p>
             </div>
             <Divider />
             <div className="welcome-btn">
+
               <Button variant="outline-secondary" block>
                 Register
               </Button>
+
             </div>
             <Divider />
-            <div className="welcome-btn">
-              <Button variant="outline-success" block>
-                Login
-              </Button>
-            </div>
-            <Divider />
+            <Dropdown eventKey="1" title="Login">
+            <Dropdown.Item eventKey="1-1" style={{paddingLeft: 0}}>
+
+            <Form>
+            <FormGroup>
+              <ControlLabel>Username</ControlLabel>
+              <FormControl name="name" />
+              <HelpBlock>Required</HelpBlock>
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Password</ControlLabel>
+              <FormControl name="password" type="password" />
+              <HelpBlock>Required</HelpBlock>
+            </FormGroup>
+            <FormGroup>
+              <ButtonToolbar>
+              <Button variant="outline-secondary" block>Submit</Button>
+            </ButtonToolbar>
+            </FormGroup>
+            </Form>
+
+        </Dropdown.Item>
+          </Dropdown>
           </Sidenav.Body>
         </Sidenav>
       </div>
