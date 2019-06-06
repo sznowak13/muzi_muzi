5b21438cac3ae0b4bea5dafe25e8700639dce63d
Szymon Nowak <sznowak13@gmail.com>
2019-06-06 18:05:09 +0200

    Changed email to be using template

--- web_app/users/signals/handlers.py
+++ web_app/users/signals/handlers.py
@@ -3,7 +3,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from web_app.utils import reverse
 from users.models import VerificationToken
-from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from muzi_muzi.utils.email import generate_mail
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -11,6 +12,5 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         token = VerificationToken.objects.create(user=instance)
         activation_url = reverse('register-verify-email', query_params={"key": token.key}, absolute=True)
-        email = instance.email
-        send_mail("Activate your Muzi Muzi account", f"Your activation link: {activation_url}",
-                  "muzi_muzi@noreply.com", [email])
+        message = render_to_string('users/email_confirmation.html', {'url': activation_url})
+        generate_mail("Activate your Muzi Muzi account", message, instance.email).send()
