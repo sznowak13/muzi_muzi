e12db25ae20eb8ce6a7423bcec8e475a8a30cedc
Banka <bachna22@gmail.com>
2019-06-18 11:22:19 +0200

    login credentals saved in login form compoonent with setstate

---
+++ front/src/sideBard/LoginForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  HelpBlock,
+  ButtonToolbar
+} from "rsuite";
+import { Button } from "react-bootstrap";
+
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.updateState = this.updateState.bind(this);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+  handleClick() {
+    console.log(this.state);
+  }
+
+  updateState(value, event) {
+    if (event.target.name === "name") {
+      this.setState({ username: value });
+    } else if (event.target.name === "password") {
+      this.setState({ password: value });
+    }
+  }
+
+  render() {
+    return (
+      <Form>
+        <FormGroup>
+          <ControlLabel>Username</ControlLabel>
+          <FormControl name="name" onChange={this.updateState} />
+          <HelpBlock>Required</HelpBlock>
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            name="password"
+            type="password"
+            onChange={this.updateState}
+          />
+          <HelpBlock>Required</HelpBlock>
+        </FormGroup>
+        <FormGroup>
+          <ButtonToolbar>
+            <Button
+              variant="outline-secondary"
+              block
+              onClick={this.handleClick}
+            >
+              Submit
+            </Button>
+          </ButtonToolbar>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
