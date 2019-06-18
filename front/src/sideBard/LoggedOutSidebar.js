73f47a027a4de232c71875c1058294c898c12f4d
Banka <bachna22@gmail.com>
2019-06-18 10:08:02 +0200

    Seperate login form from LoggedOutSidebar

--- front/src/sideBard/LoggedOutSidebar.js
+++ front/src/sideBard/LoggedOutSidebar.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { Sidenav, Icon, Divider, Dropdown } from "rsuite";
 import { Button } from "react-bootstrap";
-import { Form, FormGroup, FormControl, ControlLabel, HelpBlock, ButtonToolbar } from 'rsuite';
-
+import LoginForm from "./LoginForm";
 
 export default class LoggedOutSidebar extends Component {
   render() {
@@ -21,40 +20,21 @@ export default class LoggedOutSidebar extends Component {
               <p>Don't have an account?</p>
               <p>It's free! </p>
               <p>Sign up and join us.</p>
-              <p>{<Icon icon="heart" />}</p>
+              <p>
+                <Icon icon="heart" />
+              </p>
             </div>
             <Divider />
             <div className="welcome-btn">
-
               <Button variant="outline-secondary" block>
                 Register
               </Button>
-
             </div>
             <Divider />
             <Dropdown eventKey="1" title="Login">
-            <Dropdown.Item eventKey="1-1" style={{paddingLeft: 0}}>
-
-            <Form>
-            <FormGroup>
-              <ControlLabel>Username</ControlLabel>
-              <FormControl name="name" />
-              <HelpBlock>Required</HelpBlock>
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Password</ControlLabel>
-              <FormControl name="password" type="password" />
-              <HelpBlock>Required</HelpBlock>
-            </FormGroup>
-            <FormGroup>
-              <ButtonToolbar>
-              <Button variant="outline-secondary" block>Submit</Button>
-            </ButtonToolbar>
-            </FormGroup>
-            </Form>
-
-        </Dropdown.Item>
-          </Dropdown>
+              <Dropdown.Item eventKey="1-1" style={{ paddingLeft: 0 }} />
+              <LoginForm />
+            </Dropdown>
           </Sidenav.Body>
         </Sidenav>
       </div>
