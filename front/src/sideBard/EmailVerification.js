9c6e7f9e86ad19d498eeb44abb9ff92a8015a9f8
Szymon Nowak <sznowak13@gmail.com>
2019-06-20 13:38:25 +0200

    Added verification route in front-end

---
+++ front/src/sideBard/EmailVerification.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Loader, Container, Content, Message } from "rsuite";
+import { sendVerificationToken } from "../registerService";
+import queryString from "query-string";
+
+export default class EmailVerification extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      verifying: true,
+      verified: false
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      verifying: true,
+      verified: false
+    });
+    const params = queryString.parse(this.props.location.search);
+    sendVerificationToken(params.key).then(verified => {
+      this.setState({ verifying: false, verified: verified });
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.state.verifying ? (
+          <Loader center backdrop content="Verifying..." vertical size="lg" />
+        ) : this.state.verified ? (
+          <Content>
+            <Message
+              showIcon
+              type="success"
+              title="Success"
+              description="Success! Your account is now activated and you can login."
+            />
+          </Content>
+        ) : (
+          <Content>
+            <Message
+              showIcon
+              type="warning"
+              title="Not verified :C"
+              description="Provided token was not verified. Check the activation link again."
+            />
+          </Content>
+        )}
+      </Container>
+    );
+  }
+}
