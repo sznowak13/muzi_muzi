5496e9e20bf74f7308a29aa19e8bb4f73e63dd7d
pKraszewska <paula.kraszewska@gmail.com>
2019-06-30 19:06:04 +0200

    buttons added to each state

--- front/src/sideBard/UserProfile.js
+++ front/src/sideBard/UserProfile.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
+import { Col, Row, Image, Button } from "react-bootstrap";
 
 export default class UserProfile extends Component {
 
@@ -9,6 +10,7 @@ export default class UserProfile extends Component {
     person_username: null,
     person_city: null,
     person_description: null,
+    person_photo: null,
     person_genres: [],
     person_professions: []
   };
@@ -23,6 +25,7 @@ export default class UserProfile extends Component {
       person_username: data.username,
       person_city: data.city,
       person_description: data.description,
+      person_photo: data.photo_url,
       person_genres: data.genres,
       person_professions: data.professions
     });
@@ -30,8 +33,48 @@ export default class UserProfile extends Component {
 
   render() {
     return (
-      <Container>
-        <div>{this.state.person_city}</div>
+      <Container className="news-container">
+        <Row className="justify-content-md-center">
+          <Col xs lg="2">
+            <Image src={this.state.person_photo} roundedCircle />
+          </Col>
+          <Col md="auto">
+            <h2 className="news-title">{this.state.person_first_name} {this.state.person_last_name}</h2>
+            <h2 className="news-title"> "{this.state.person_username}" </h2>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={{ span: 6, offset: 2 }} className="info-title">
+            <Button variant="light" size="sm">
+              <i className="fas fa-edit" />
+            </Button>{" "}
+            City:
+          </Col>
+          <Col md={{ span: 6, offset: 2 }}>{this.state.person_city}</Col>
+          <Col md={{ span: 6, offset: 2 }} className="info-title">
+            <Button variant="light" size="sm">
+              <i className="fas fa-edit" />
+            </Button>{" "}
+            Description:
+          </Col>
+          <Col md={{ span: 6, offset: 2 }}>{this.state.person_description}</Col>
+          <Col md={{ span: 6, offset: 2 }} className="info-title">
+            <Button variant="light" size="sm">
+              <i className="fas fa-edit" />
+            </Button>{" "}
+            Genres:
+          </Col>
+          <Col md={{ span: 6, offset: 2 }}>{this.state.person_genres}</Col>
+          <Col md={{ span: 6, offset: 2 }} className="info-title">
+            <Button variant="light" size="sm">
+              <i className="fas fa-edit" />
+            </Button>{" "}
+            Professions:
+          </Col>
+          <Col md={{ span: 6, offset: 2 }}>{this.state.person_professions}</Col>
+        </Row>
+
       </Container>
   )}
 }
