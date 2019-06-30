661f83b679cc29d638cf03bba5ccbe306ece0887
pKraszewska <paula.kraszewska@gmail.com>
2019-06-30 17:44:55 +0200

    fetching user form api and save date in state

--- front/src/sideBard/UserProfile.js
+++ front/src/sideBard/UserProfile.js
@@ -1,7 +1,37 @@
-import React from "react";
-class UserProfile extends React.Component {
-  render() {
-    return <h1>Users</h1>;
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+
+export default class UserProfile extends Component {
+
+  state = {
+    person_first_name: null,
+    person_last_name: null,
+    person_username: null,
+    person_city: null,
+    person_description: null,
+    person_genres: [],
+    person_professions: []
+  };
+
+  async componentDidMount() {
+    const url = "http://127.0.0.1:8000/users/1";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({
+      person_first_name: data.first_name,
+      person_last_name: data.last_name,
+      person_username: data.username,
+      person_city: data.city,
+      person_description: data.description,
+      person_genres: data.genres,
+      person_professions: data.professions
+    });
   }
+
+  render() {
+    return (
+      <Container>
+        <div>{this.state.person_city}</div>
+      </Container>
+  )}
 }
-export default UserProfile;
