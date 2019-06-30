83315b332c34e49a1fed092370e004be44774c30
pKraszewska <paula.kraszewska@gmail.com>
2019-06-30 17:44:16 +0200

    disabled not found page

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -8,14 +8,14 @@ import "antd/dist/antd.css";
 import "../node_modules/rsuite/dist/styles/rsuite.min.css";
 import "./style/index.css";
 import UserProfile from "./sideBard/UserProfile";
-import Notfound from "./notFound";
+// import Notfound from "./notFound";
 
 const routing = (
   <Router>
     <div>
-      <Route path="/" component={App} />
-      <Route path="profile" component={UserProfile} />
-      <Route component={Notfound} />
+      <Route exact path="/" component={App} />
+      <Route path="/profile/:id" component={UserProfile} />
+      {/*<Route component={Notfound} />*/}
     </div>
   </Router>
 );
