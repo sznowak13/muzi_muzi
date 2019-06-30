b3b0c95757e648dbf75a857403497fc19c001134
pKraszewska <paula.kraszewska@gmail.com>
2019-06-30 13:11:36 +0200

    routing created with react ruoter dom for main page and user prifile

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import "../node_modules/rsuite/dist/styles/rsuite.min.css";
 import "./style/index.css";
+import UserProfile from "./sideBard/UserProfile";
+import Notfound from "./notFound";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+      <Route path="profile" component={UserProfile} />
+      <Route component={Notfound} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
+
+// ReactDOM.render(<App />, document.getElementById("root"));
