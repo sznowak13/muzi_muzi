169f8e1ca09e88ff3b74946892cdbd66f5859430
pKraszewska <paula.kraszewska@gmail.com>
2019-07-02 09:00:44 +0200

    router deleted'

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -8,13 +7,4 @@ import "antd/dist/antd.css";
 import "../node_modules/rsuite/dist/styles/rsuite.min.css";
 import "./style/index.css";
 
-const routing = (
-  <Router>
-    <div>
-      <Route path="/" component={App} />
-    </div>
-  </Router>
-);
-
-ReactDOM.render(routing, document.getElementById("root"));
-
+ReactDOM.render(<App />, document.getElementById("root"));
