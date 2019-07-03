2a305814420eb35e5475ebeaf8053f36ddb3ba4b
Szymon Nowak <sznowak13@gmail.com>
2019-07-03 20:06:35 +0200

    Started FIGHTING with profile edit
    added utility functions for optimazation

--- front/src/sideBard/UserProfile.js
+++ front/src/sideBard/UserProfile.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
-import { Col, Row, Image } from "react-bootstrap";
+import { Image } from "react-bootstrap";
+import { Input, InputGroup, Icon, Col, Row, Container } from "rsuite";
 
 export default class UserProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      person_id: "",
-      person_first_name: "",
-      person_last_name: "",
-      person_username: "",
-      person_city: "",
-      person_description: "",
-      person_photo: "",
-      person_email: "",
-      person_genres: [],
-      person_professions: []
+      fields_map: {
+        person_id: "user_id",
+        person_first_name: "first_name",
+        person_last_name: "last_name",
+        person_username: "username",
+        person_city: "city",
+        person_description: "description",
+        person_photo: "photo_url",
+        person_email: "email",
+        person_genres: "genres",
+        person_professions: "professions"
+      },
+      person_id: { edit: false, value: "" },
+      person_first_name: { edit: false, value: "" },
+      person_last_name: { edit: false, value: "" },
+      person_username: { edit: false, value: "" },
+      person_city: { edit: false, value: "" },
+      person_description: { edit: false, value: "" },
+      person_photo: { edit: false, value: "" },
+      person_email: { edit: false, value: "" },
+      person_genres: { edit: false, value: [] },
+      person_professions: { edit: false, value: "" }
     };
   }
 
@@ -24,19 +36,65 @@ export default class UserProfile extends Component {
       "muzi_muzi_user_id"
     )}/`;
     const response = await fetch(url);
-    const data = await response.json();
-    this.setState({
-      person_id: data.user_id,
-      person_first_name: data.first_name,
-      person_last_name: data.last_name,
-      person_username: data.username,
-      person_city: data.city,
-      person_description: data.description,
-      person_photo: data.photo_url,
-      person_email: data.email,
-      person_genres: data.genres,
-      person_professions: data.professions
-    });
+    let data = await response.json();
+    data = this.formatFetchData(data);
+    this.setState(data);
+  }
+
+  formatFetchData(data) {
+    let formated = {};
+    for (let entry of Object.entries(this.state.fields_map)) {
+      formated[entry[0]] = { ...this.state[entry[0]], value: data[entry[1]] };
+    }
+    console.log(formated);
+    return formated;
+  }
+
+  formatFieldDisplay(fieldName) {
+    let value = this.state[fieldName].value;
+    if (!value) {
+      return "---";
+    } else if (Array.isArray(value)) {
+      return value.join(", ");
+    } else {
+      return value;
+    }
+  }
+
+  toggleEdit(fieldName) {
+    return () => {
+      this.setState({
+        [fieldName]: {
+          ...this.state[fieldName],
+          edit: !this.state[fieldName].edit
+        }
+      });
+    };
+  }
+
+  checkIfEdit(fieldName, label) {
+    let body;
+    let labelElement = <Col className="person-info-title">{label}</Col>;
+    if (this.state[fieldName].edit) {
+      body = (
+        <InputGroup style={{ width: 300 }} onBlur={this.toggleEdit(fieldName)}>
+          <Input value={this.state[fieldName].value} />
+          <InputGroup.Button>
+            <Icon icon="check-square" />
+          </InputGroup.Button>
+        </InputGroup>
+      );
+    } else {
+      body = (
+        <Row>
+          <Col>{this.formatFieldDisplay(fieldName)}</Col>
+          <Col className="edit-button" onClick={this.toggleEdit(fieldName)}>
+            Edit
+          </Col>
+        </Row>
+      );
+    }
+    return [<Row>{labelElement}</Row>, body];
   }
 
   render() {
@@ -49,7 +107,7 @@ export default class UserProfile extends Component {
                 width="200"
                 height="200"
                 className="person-photo"
-                src={this.state.person_photo}
+                src={this.state.person_photo.value}
                 roundedCircle
               />
             </Col>
@@ -57,9 +115,13 @@ export default class UserProfile extends Component {
           <Row>
             <Col>
               <h2 className="news-title">
-                {this.state.person_first_name} {this.state.person_last_name}
+                {this.state.person_first_name.value}{" "}
+                {this.state.person_last_name.value}
               </h2>
-              <h2 className="news-title"> "{this.state.person_username}" </h2>
+              <h2 className="news-title">
+                {" "}
+                "{this.state.person_username.value}"{" "}
+              </h2>
             </Col>
           </Row>
         </Container>
@@ -69,14 +131,14 @@ export default class UserProfile extends Component {
             <Col className="person-info-title">Email:</Col>
           </Row>
           <Row>
-            <Col>{this.state.person_email}</Col>
+            <Col>{this.state.person_email.value}</Col>
           </Row>
 
           <Row>
             <Col className="person-info-title">City:</Col>
           </Row>
           <Row>
-            <Col>{this.state.person_city}</Col>
+            <Col>{this.state.person_city.value}</Col>
           </Row>
 
           <Row>
@@ -84,8 +146,8 @@ export default class UserProfile extends Component {
           </Row>
           <Row>
             <Col>
-              {this.state.person_professions.length > 0 ? (
-                this.state.person_professions.toString()
+              {this.state.person_professions.value.length > 0 ? (
+                this.state.person_professions.value.toString()
               ) : (
                 <span>Not provided</span>
               )}
@@ -97,8 +159,8 @@ export default class UserProfile extends Component {
           </Row>
           <Row>
             <Col>
-              {this.state.person_genres.length > 0 ? (
-                this.state.person_genres.toString()
+              {this.state.person_genres.value.length > 0 ? (
+                this.state.person_genres.value.toString()
               ) : (
                 <span>Not provided</span>
               )}
@@ -111,7 +173,7 @@ export default class UserProfile extends Component {
             <Col className="person-info-title">Description:</Col>
           </Row>
           <Row>
-            <Col>{this.state.person_description}</Col>
+            <Col>{this.state.person_description.value}</Col>
           </Row>
         </Container>
       </Container>
