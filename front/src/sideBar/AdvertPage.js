90ae5506212c530c23add755d9330e97eb35b09e
pKraszewska <paula.kraszewska@gmail.com>
2019-07-03 17:03:46 +0200

    added flexboxes for image and title, desc and posted

--- front/src/sideBar/AdvertPage.js
+++ front/src/sideBar/AdvertPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
-import Flexbox from 'flexbox-react';
-import { Col, Row, Image } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
+import Flexbox from "flexbox-react";
+import genericband from "../resources/generic-band.jpg";
+import FlexboxGridItem from "rsuite/lib/FlexboxGridItem";
 
 export default class UserProfile extends Component {
   constructor(props) {
@@ -39,11 +40,37 @@ export default class UserProfile extends Component {
   render() {
     return (
       <Container className="news-container">
-        <Flexbox flexDirection="row" justifyContent="space-around" margin="1%">
+        <Flexbox flexDirection="row" justifyContent="space-around" margin="1%" padding="3%">
           <div className="advert-header"> BAND </div>
           <div className="advert-header"> {this.state.advert_city} </div>
           <div className="advert-header"> {this.state.advert_profession} </div>
           <div className="advert-header"> {this.state.advert_genre}</div>
+        </Flexbox>
+        <Flexbox
+          flexDirection="row"
+          justifyContent="space-around"
+          margin="1%"
+          padding="3%"
+        >
+          <div> kiedys bedzie image</div>
+          <div className="news-title"> {this.state.advert_title} </div>
+        </Flexbox>
+        <Flexbox
+          flexDirection="row"
+          justifyContent="center"
+          margin="1%"
+          padding="3%"
+        >
+          <div className="advert-description">
+            {" "}
+            {this.state.advert_description}{" "}
+          </div>
+        </Flexbox>
+        <Flexbox flexDirection="row" justifyContent="space-evenly" margin="1%" padding="3%">
+          <div className="advert-description"> Posted by: </div>
+          <div> {this.state.advert_username} </div>
+          <div className="advert-description"> Posted on: </div>
+          <div> {this.state.advert_posted_on} </div>
         </Flexbox>
       </Container>
     );
