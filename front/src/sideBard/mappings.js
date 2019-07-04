8b743b621baf5996f91601a1aeb2f53489d2b924
Szymon Nowak <sznowak13@gmail.com>
2019-07-04 21:20:59 +0200

    Forgotten file xD

---
+++ front/src/sideBard/mappings.js
@@ -0,0 +1,12 @@
+export const userProfileFiledMapping = {
+  person_id: "user_id",
+  person_first_name: "first_name",
+  person_last_name: "last_name",
+  person_username: "username",
+  person_city: "city",
+  person_description: "description",
+  person_photo: "photo_url",
+  person_email: "email",
+  person_genres: "genres",
+  person_professions: "professions"
+};
