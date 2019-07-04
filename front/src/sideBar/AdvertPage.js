0e124d1a8d6901a87dce011e135ed40d55e63b37
pKraszewska <paula.kraszewska@gmail.com>
2019-07-04 12:15:47 +0200

    changed advert info to tags

--- front/src/sideBar/AdvertPage.js
+++ front/src/sideBar/AdvertPage.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 import { Container, Image } from "react-bootstrap";
 import Flexbox from "flexbox-react";
 import Button from "react-bootstrap/Button";
+import { Divider } from "rsuite";
+import UsersSneakPeak from "../mainContent/UsersSneakPeak";
+import userphoto from "../resources/userphoto.png";
+import genericband from "../resources/generic-band.jpg";
+import { Link } from "react-router-dom";
+import { Tag, TagGroup } from "rsuite";
 
-export default class UserProfile extends Component {
+export default class AdvertPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       advert_id: "",
       advert_username: "",
+      advert_user_id: "",
       advert_city: "",
       advert_band_name: null,
       advert_title: "",
@@ -20,12 +27,13 @@ export default class UserProfile extends Component {
   }
 
   async componentDidMount() {
-    const url = `http://127.0.0.1:8000/adverts/10/`;
+    const url = `http://127.0.0.1:8000/adverts/${this.props.match.params.id}/`;
     const response = await fetch(url);
     const data = await response.json();
     this.setState({
       advert_id: data.advert_id,
       advert_username: data.username,
+      advert_user_id: data.user_id,
       advert_city: data.city,
       advert_band_name: data.band_name,
       advert_title: data.title,
@@ -34,10 +42,6 @@ export default class UserProfile extends Component {
       advert_genre: data.genre,
       advert_profession: data.profession
     });
-  }
-
-  goToUserPage() {
-      console.log("jestem")
   }
 
   render() {
@@ -49,26 +53,42 @@ export default class UserProfile extends Component {
           margin="1%"
           padding="3%"
         >
-          <div className="advert-header">
-            {" "}
-            {this.state.advert_band_name != null ? (
-              <span> BAND </span>
-            ) : (
-              <span> USER </span>
-            )}{" "}
-          </div>
-          <div className="advert-header"> {this.state.advert_city} </div>
-          <div className="advert-header"> {this.state.advert_profession} </div>
-          <div className="advert-header"> {this.state.advert_genre}</div>
+          <TagGroup>
+            <Tag color="green" className="advert-header">
+              {" "}
+              {this.state.advert_band_name != null ? (
+                <span> BAND </span>
+              ) : (
+                <span> USER </span>
+              )}{" "}
+            </Tag>
+            <Tag color="green" className="advert-header">
+              {this.state.advert_city}
+            </Tag>
+            <Tag color="green" className="advert-header">
+              {this.state.advert_profession}
+            </Tag>
+            <Tag color="green" className="advert-header">
+              {this.state.advert_genre}
+            </Tag>
+          </TagGroup>
         </Flexbox>
+        <Divider />
         <Flexbox
           flexDirection="row"
           justifyContent="space-around"
           margin="1%"
           padding="3%"
         >
-          <div> kiedys bedzie image</div>
-          <div className="news-title"> {this.state.advert_title} </div>
+          <div>
+            {" "}
+            {this.state.advert_band_name != null ? (
+              <img className="advert-photo" src={genericband} alt="Logo" />
+            ) : (
+              <img className="advert-photo" src={userphoto} alt="Logo" />
+            )}{" "}
+          </div>
+          <div className="advert-page-title"> {this.state.advert_title} </div>
         </Flexbox>
         <Flexbox
           flexDirection="row"
@@ -88,9 +108,13 @@ export default class UserProfile extends Component {
           padding="3%"
         >
           <div className="advert-posted"> Posted by: </div>
-          <div className="advert-posted" onClick={this.goToUserPage}>
-            {" "}
-            {this.state.advert_username}{" "}
+          <div className="advert-posted">
+            <Link
+              className="links"
+              to={`../profile/${this.state.advert_user_id}`}
+            >
+              {this.state.advert_username}
+            </Link>
           </div>
           <div className="advert-posted"> Posted on: </div>
           <div className="advert-posted"> {this.state.advert_posted_on} </div>
