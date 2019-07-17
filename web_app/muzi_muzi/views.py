b900d3e5dfeff5f1a5c384cbfe16735f394810e8
Szymon Nowak <sznowak13@gmail.com>
2019-07-17 20:13:29 +0200

    Added ListAll mixin for Genre and Profession
    it returns all data from db

--- web_app/muzi_muzi/views.py
+++ web_app/muzi_muzi/views.py
@@ -6,34 +6,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 
 
-class ObtainTokenAndId(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        resp = super().post(request, *args, **kwargs)
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        resp.data['id'] = user.user_id
-        return resp
-
-
-class ProfessionList(viewsets.ModelViewSet):
-    queryset = Profession.objects.all()
-    serializer_class = ProfessionSerializer
-
-
-class GenreList(viewsets.ModelViewSet):
-    queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
-
-
-class CityList(viewsets.ModelViewSet):
-    queryset = City.objects.all()
-    serializer_class = CitySerializer
-
-
 class LatestViewMixIn(object):
-
     """
     Custom mixin for /latest endpoint, returning a list of models ordered by some creation date.
     By default it uses 'created' column in model and 'num' query param for parametrization; returns 10
@@ -48,7 +21,45 @@ class LatestViewMixIn(object):
     def latest(self, request):
         latest_num = request.query_params.get(self.latest_param, self.latest_num)
         if not isinstance(latest_num, int) and not latest_num.isdigit():
-            return Response({'error': f'Wrong {self.latest_param} parameter, was not a number. ({self.latest_param}={latest_num})'})
+            return Response(
+                {'error': f'Wrong {self.latest_param} parameter, was not a number. ({self.latest_param}={latest_num})'})
         queryset = self.get_queryset().order_by('-' + self.latest_by)[:int(latest_num)]
         serializer = self.serializer_class(queryset, many=True, context={'request': request})
         return Response(serializer.data)
+
+
+class ListAllMixIn(object):
+    """
+    Custom mixin for /all endpoint that returns all data without pagination.
+    """
+
+    @action(detail=False)
+    def all(self, request):
+        serializer = self.serializer_class(self.get_queryset(), many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+class ObtainTokenAndId(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        resp = super().post(request, *args, **kwargs)
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        resp.data['id'] = user.user_id
+        return resp
+
+
+class ProfessionList(viewsets.ModelViewSet, ListAllMixIn):
+    queryset = Profession.objects.all()
+    serializer_class = ProfessionSerializer
+
+
+class GenreList(viewsets.ModelViewSet, ListAllMixIn):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CityList(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
